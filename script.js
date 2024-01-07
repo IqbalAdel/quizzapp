2dc3a0900e5e84158a69084c85383f35a571fa60
Iqbal Adel <IAdel@gmx.de>
2024-01-07 10:12:36 -0800

    add: JSON questions

---
+++ script.js
@@ -0,0 +1,46 @@
+let questions = [
+    {
+        "question":"Was ist der längste Fluss der Welt",
+        "answer_1":"Der Amazonas",
+        "answer_2":"Der Nil",
+        "answer_3":"Der Rhein",
+        "answer_4":"Der Niger",
+        "right_answer":"2",
+    },
+    
+    {
+        "question":"Was ist die meist gesprochene Sprache in Indien",
+        "answer_1":"Hindi",
+        "answer_2":"Urdu",
+        "answer_3":"Punjabi",
+        "answer_4":"Bengali",
+        "right_answer":"1",
+    },
+
+    {
+        "question":"In welchem dieser Filme spielt Leonardo DiCaprio NICHT mit",
+        "answer_1":"The Wolf of Wallstreet",
+        "answer_2":"12 Years a Slave",
+        "answer_3":"Aviator",
+        "answer_4":"The Revenant",
+        "right_answer":"2",
+    },
+
+    {
+        "question":"Was verbirgt sich hinter einer Konklave",
+        "answer_1":"Die Befreiung der Sklaverei in den Südstaaten",
+        "answer_2":"Eine Papstwahl",
+        "answer_3":"Ein Musikstück",
+        "answer_4":"Eine besondere Form der Kunst",
+        "right_answer":"2",
+    },
+
+    {
+        "question":"Welche Blumenzwiebeln wurden früher als Zahlungsmittel genutzt",
+        "answer_1":"Krokusse",
+        "answer_2":"Lilien",
+        "answer_3":"Gladiolen",
+        "answer_4":"Tulpen",
+        "right_answer":"4",
+    },
+];
